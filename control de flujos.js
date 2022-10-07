3788679449b4cdc640f3f877ff86a35eb0111838
Alexis Franco <ronaldofranco196@gmail.com>
2022-10-06 22:43:59 -0300

    control de flujos

---
+++ control de flujos.js
@@ -0,0 +1,23 @@
+// Bucle for
+
+let num = 1
+
+for (let i = 1; i <= 10; i++){
+    num = num * i;
+    console.log(num);
+}
+
+
+// Bucle while
+
+let factorial = 10;
+let nro = 1 
+
+while(true){
+    factorial *= nro;
+    if(nro > 10){
+        break;
+    }
+    nro++;
+    console.log(factorial);
+}
